1bce8f0aecf547ea75160d388d6b5c2cccf4325b
Edmar Zatarin <edzatarin@gmail.com>
2024-05-16 20:31:24 -0300

    Testes capturar CAR na Infosimples para desenvolvimento dos LOGS do CAR

--- src/async/api_async.py
+++ src/async/api_async.py
@@ -1,14 +1,19 @@
 import base64
+import uuid
 import requests
 import json
 
 url = 'https://api.infosimples.com/api-async/v2/car/demonstrativo'
 
+transaction_id = str(uuid.uuid4())
+context = f"createdby={139};updatedby={139};transactionid={transaction_id}"
+encode_context = base64.b64encode(context.encode("ascii"))
+
 args = {
-  "car": "MT-5104526-D227623ED4654F298AAE9632847245E4",
+  "car": "MT-5102637-4AF7AA48774144498AD3039DD377D764",
   "token": "NuUBrhgD5v4nIWfb4bgrmXNwg7AALulkSq-BsYk3",
   "callback_url": "http://192.168.1.112:8000/CAR",
-  "context": base64.b64encode("MT-5104526-D227623ED4654F298AAE9632847245E4".encode("ascii")),
+  "context": str(encode_context),
   "timeout": 300
 }
 
