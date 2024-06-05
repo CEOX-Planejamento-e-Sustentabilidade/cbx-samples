98b582e3894a2fbaf96996209255e3235fb32392
Edmar Zatarin <edzatarin@gmail.com>
2024-06-05 13:35:42 -0300

    Testes consultar SIGEF infosimples

--- src/infosimples/parcelas.py
+++ src/infosimples/parcelas.py
@@ -7,14 +7,34 @@
 #Matrícula Alvorada
 #INCRA 9240160015113
 
+#22068769816
+#Senha@123
+
 import requests
 import json
 
-url = 'https://api.infosimples.com/api/v2/consultas/status/incra/sigef/parcelas'
+url = 'https://api.infosimples.com/api/v2/consultas/incra/sigef/parcelas'
 args = {
-  "codigo_imovel": "9240160015113",
+  #"cpf":           "VALOR_DO_PARAMETRO_CPF",
+  #"cnpj":          "VALOR_DO_PARAMETRO_CNPJ",
+  #"pagina":        "VALOR_DO_PARAMETRO_PAGINA",  
+  "login_cpf":     "22068769816",
+  "login_senha":   "Senha@123",  
+  "codigo_imovel": "6270200045610",
   "token": "NuUBrhgD5v4nIWfb4bgrmXNwg7AALulkSq-BsYk3"
 }
+
+  # "cpf":           "VALOR_DO_PARAMETRO_CPF",
+  # "cnpj":          "VALOR_DO_PARAMETRO_CNPJ",
+  # "codigo_imovel": "VALOR_DO_PARAMETRO_CODIGO_IMOVEL",
+  # "pagina":        "VALOR_DO_PARAMETRO_PAGINA",
+  # "login_cpf":     "VALOR_DO_PARAMETRO_LOGIN_CPF",
+  # "login_senha":   "VALOR_DO_PARAMETRO_LOGIN_SENHA",
+  # #"pkcs12_cert":   aes256.encrypt(base64.b64encode(open("certificado.pfx", "rb").read()).decode(), "INFORME_A_CHAVE_DE_CRIPTOGRAFIA"),
+  # #"pkcs12_pass":   aes256.encrypt("SENHA_DO_CERTIFICADO", "INFORME_A_CHAVE_DE_CRIPTOGRAFIA"),
+  # "token":         "INFORME_AQUI_O_TOKEN_DA_CHAVE_DE_ACESSO",
+  # "timeout":       300
+
 
 response = requests.post(url, args)
 response_json = response.json()
