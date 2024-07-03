a96d68ca70166d0732a1c0cd6288e409921f4c61
Edmar Zatarin <edzatarin@gmail.com>
2024-07-03 18:32:54 -0300

    Refactoring atualizacao das fontes dos CARs que pertencem ao Shapefile

--- src/scripts/upload_shapefile_from_dbf.py
+++ src/scripts/upload_shapefile_from_dbf.py
@@ -90,34 +90,102 @@ def main():
             print(f'erro: {ex.args}')
     
     try:
-    
-        # ADICIONAR: car que estao na tabela shapefile mas nao estao na tabela car_input
-        insert_statement = """ 
-            insert into cbx.car_input (nr_car, cpf_cnpj, nome_proprietario, status, 
-            clients, 
-            sources, 
-            created_at, updated_at, created_by, updated_by)
-            select distinct 
-                shp.nr_car, shp.cpf_cnpj , '' as nome_proprietario, true as status, 
-                COALESCE(ci.clients::jsonb, '[]'::jsonb) || '[{"id_client": 1}]'::jsonb as clients, 
-                COALESCE(ci.sources::jsonb, '[]'::jsonb) || '[{"id_source": 6}]'::jsonb as sources,
-                current_timestamp, current_timestamp, 139, 139
-            from cbx.shapefile shp
-            inner join cbx.car_input ci on shp.nr_car = ci.nr_car and shp.cpf_cnpj = ci.cpf_cnpj
-            where ci.nr_car is null; 
+        reset_statement = """
+        -- deleta todos os cars com source puramente de 'shapefile'
+        delete from cbx.car_input WHERE sources::jsonb = '[{"id_source": 6}]'::jsonb;        
+        
+        /* reset sources sem shapefile, desconsidera todos as fontes 'shapefile'
+        ex.:
+        para sources = [{"id_source": 1}, {"id_source": 6}] 
+        se torna = [{"id_source": 1}] 
+        */
+
+        DO $$ 
+        declare
+            total_rows integer;    
+            iterations integer;
+            offsetx integer;
+            chunk_size integer := 1000; -- Adjust the chunk size as needed   
+        BEGIN
+            -- Count total rows that need updating  
+            select count(*) 
+            into total_rows
+            from
+            (
+                SELECT filtered.id
+                FROM (
+                    SELECT ci.id, jsonb_array_elements(ci.sources::jsonb) AS elem
+                    FROM cbx.car_input ci
+                ) AS filtered	    
+                WHERE filtered.elem->>'id_source' <> '6'
+                group by filtered.id        
+            ) x;
+
+            -- Calculate number of iterations
+            iterations := CEIL(total_rows / chunk_size);
+            
+            -- Process updates in batches
+            FOR i IN 0 .. iterations - 1 loop
+                begin
+                    offsetx := i * chunk_size;
+            
+                    UPDATE cbx.car_input AS ci
+                    SET sources = subq.sources
+                    FROM (
+                        SELECT filtered.id, jsonb_agg(filtered.elem) as sources
+                        FROM (
+                            SELECT ci.id, jsonb_array_elements(ci.sources::jsonb) AS elem
+                            FROM cbx.car_input ci
+                        ) AS filtered	    
+                        WHERE filtered.elem->>'id_source' <> '6'
+                        group by filtered.id
+                        OFFSET offsetx LIMIT chunk_size 
+                    ) AS subq
+                    WHERE ci.id = subq.id
+                    and ci.sources::jsonb @> '[{"id_source": 6}]'::jsonb;
+                EXCEPTION
+                    WHEN OTHERS THEN
+                        -- Rollback in case of error
+                        ROLLBACK;
+                        RAISE EXCEPTION 'Error occurred: %', SQLERRM;
+                end;
+            END loop;
+        END $$;        
+        """
+        #cur.execute(reset_statement)
+        
+        insert_statement = """                 
+        -- adicionar cars que nao tem na ci mas tem na shp (cars com cpf)
+        insert into cbx.car_input (nr_car, cpf_cnpj, nome_proprietario, status, 
+        clients, 
+        sources, 
+        created_at, updated_at, created_by, updated_by)
+        select distinct 
+            shp.nr_car, shp.cpf_cnpj , '' as nome_proprietario, true as status, 
+            COALESCE(ci.clients::jsonb, '[]'::jsonb) || '[{"id_client": 1}]'::jsonb as clients, 
+            COALESCE(ci.sources::jsonb, '[]'::jsonb) || '[{"id_source": 6}]'::jsonb as sources,
+            current_timestamp, current_timestamp, 139, 139
+        from cbx.shapefile shp
+        left join cbx.car_input ci on shp.nr_car = ci.nr_car and shp.cpf_cnpj = ci.cpf_cnpj
+        where ci.nr_car is null 
+        and ci.cpf_cnpj is null
+        and shp.nr_car <> ''
+        and shp.nr_car <> 'NÃO TEM CAR';
+
+        -- atualizar car que estao em shp e ci, mas nao tem cpf no ci
+        update cbx.car_input ci SET
+            cpf_cnpj = s.cpf_cnpj
+            ,sources = coalesce(sources::jsonb, '[]'::jsonb) || '[{"id_source": 6}]'::jsonb
+        from cbx.shapefile s
+        where s.nr_car = ci.nr_car
+        and s.cpf_cnpj is not null
+        and ci.cpf_cnpj is null
+        and s.nr_car <> 'NÃO TEM CAR'
+        and s.nr_car <> ''
+        and not ci.sources::jsonb @> '[{"id_source": 6}]'::jsonb;
         """
         cur.execute(insert_statement)
-
-        # ATUALIZAR: car que estao na duas tabelas
-        update_statement = """
-            UPDATE cbx.car_input as ci SET 
-                sources = COALESCE(ci.sources::jsonb, '[]'::jsonb) || '[{"id_source": 6}]'::jsonb	
-            from cbx.shapefile as shp 
-            where ci.nr_car = shp.nr_car and ci.cpf_cnpj = shp.cpf_cnpj
-            and ci.sources::jsonb <> '[{"id_source": 6}]'
-        """            
-        cur.execute(update_statement)
-        
+       
         conn.commit()
     except Exception as e:
         conn.rollback()
